2f96c86c9ea2e9c0dc8fc24ffa564811c2371b04
Zacharia Ndinguri <zacthuku@gmail.com>
2025-05-10 09:35:44 +0300

    attributes and properties

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,26 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    def __init__(self, name="Dog", breed="Chihuahua"):
+        self.name=name
+        self.breed=breed
+    def get_name(self):
+        print('getting name')
+        return self._name
+    def set_name(self, name=""):
+        if isinstance (name, str) and 1 <= len(name) <= 25:
+            self._name=name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    name=property(get_name, set_name)
+
+
+    def get_breed(self):
+        return self._breed 
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed=breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    breed=property(get_breed, set_breed)        
     pass
